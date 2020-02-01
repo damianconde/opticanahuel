b2d9c308538515c1d65b416b21bf5204af667d34
damian conde <damian@peakonestudios.com>
2020-02-01 20:30:23 -0300

    Init 2

--- Infrastructure.IoC/InfrastructureModule.cs
+++ Infrastructure.IoC/InfrastructureModule.cs
@@ -1,4 +1,6 @@
-﻿using Domain.Contracts;
+﻿using AfipLoginService;
+using AfipServiceClient;
+using Domain.Contracts;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -11,6 +13,8 @@ namespace Infrastructure.IoC
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddSingleton<IAfipService, AfipService>();
+            services.AddSingleton<ServiceSoap>(o => new ServiceSoapClient(ServiceSoapClient.EndpointConfiguration.ServiceSoap12));
+            services.AddSingleton<LoginCMS>(o => new LoginCMSClient(LoginCMSClient.EndpointConfiguration.LoginCms));
 
             return services;
         }
--- Infrastructure/AfipService.cs
+++ Infrastructure/AfipService.cs
@@ -1,4 +1,6 @@
-﻿using Domain.Contracts;
+﻿using AfipLoginService;
+using AfipServiceClient;
+using Domain.Contracts;
 using Domain.Models;
 using System;
 using System.Collections.Generic;
@@ -8,9 +10,57 @@ namespace Infrastructure
 {
     public class AfipService : IAfipService
     {
+        private readonly ServiceSoap afipServiceClient;
+        private readonly LoginCMS afipLogin;
+
+        public AfipService(ServiceSoap afipServiceClient, LoginCMS afipLogin)
+        {
+            this.afipServiceClient = afipServiceClient;
+            this.afipLogin = afipLogin;
+        }
+
         public async Task<IEnumerable<Voucher>> GetVouchers(DateTime from, DateTime to)
         {
-            throw new NotImplementedException();
+            string loginCmsBase64 = null;
+
+            var auth = await afipLogin.loginCmsAsync(new loginCmsRequest
+            {
+                in0 = loginCmsBase64
+            });
+
+            await afipServiceClient.FECompConsultarAsync(new FECompConsultarRequest { 
+                Body = new FECompConsultarRequestBody
+                {
+                    Auth = new FEAuthRequest
+                    {
+                        Cuit = 20308592295,
+                        Sign = "",
+                        Token = ""
+                    },
+                    FeCompConsReq = new FECompConsultaReq
+                    {
+                        
+                    }
+                }
+            });
+
+            //await afipServiceClient.
+
+            var response = await afipServiceClient.FECompTotXRequestAsync(new FECompTotXRequestRequest
+            {
+                Body = new FECompTotXRequestRequestBody
+                {
+                    Auth = new FEAuthRequest
+                    {
+                        Cuit = 20308592295,
+                        Sign = "",
+                        Token = ""
+                    }
+                }                
+            });
+
+
+            return null;
         }
     }
 }
